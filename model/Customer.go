ece2f30a81f68ae4c33b673d3c76a24320b7b538
Mehdi <mehditeymorian322@gmail.com>
2020-12-28 18:28:02 +0330

    fix var type

--- model/Customer.go
+++ model/Customer.go
@@ -1,11 +1,14 @@
 package model
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type Customer struct {
-	Name         string    `json:"c_name"`
-	Telephone    int32     `json:"cTel"`
-	Address      string    `json:"cAddress"`
-	Id           int       `json:"cId"`
-	RegisterDate time.Time `json:"cRegisterDate"`
+	Name         string      `json:"c_name"`
+	Telephone    json.Number `json:"cTel"`
+	Address      string      `json:"cAddress"`
+	Id           json.Number `json:"cId"`
+	RegisterDate time.Time   `json:"cRegisterDate"`
 }
