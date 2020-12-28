7e21a826c777e634ba544badfdd7d251f76e8d28
Mehdi <mehditeymorian322@gmail.com>
2020-12-28 22:02:04 +0330

    change registerDate to string / create response struct for insert and retrieveAll

--- model/Customer.go
+++ model/Customer.go
@@ -1,14 +1,24 @@
 package model
 
-import (
-	"encoding/json"
-	"time"
-)
-
 type Customer struct {
-	Name         string      `json:"c_name"`
-	Telephone    json.Number `json:"cTel"`
-	Address      string      `json:"cAddress"`
-	Id           json.Number `json:"cId"`
-	RegisterDate time.Time   `json:"cRegisterDate"`
+	Name         string `json:"cName"`
+	Telephone    int64  `json:"cTel"`
+	Address      string `json:"cAddress"`
+	Id           int64  `json:"cId"`
+	RegisterDate string `json:"cRegisterDate"`
+}
+
+type CustomerInsertResp struct {
+	Name         string `json:"cName"`
+	Telephone    int64  `json:"cTel"`
+	Address      string `json:"cAddress"`
+	Id           int64  `json:"cId"`
+	RegisterDate string `json:"cRegisterDate"`
+	Msg          string `json:"msg"`
+}
+
+type CustomerRetrieveResp struct {
+	Size      int64      `json:"size"`
+	Customers []Customer `json:"customers"`
+	Msg       string     `json:"msg"`
 }
