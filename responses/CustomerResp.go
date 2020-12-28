913947878e2e5c3b817993564a78f800d0ac64e1
Mehdi <mehditeymorian322@gmail.com>
2020-12-28 22:03:36 +0330

    Create response struct for given values

---
+++ responses/CustomerResp.go
@@ -0,0 +1,24 @@
+package responses
+
+import (
+	"GoServer/model"
+)
+
+func EchoResp(c model.Customer, msg string) model.CustomerInsertResp {
+	return model.CustomerInsertResp{
+		Name:         c.Name,
+		Telephone:    c.Telephone,
+		Address:      c.Address,
+		Id:           c.Id,
+		RegisterDate: c.RegisterDate,
+		Msg:          msg,
+	}
+}
+
+func RetrieveAllResp(c []model.Customer, msg string) model.CustomerRetrieveResp {
+	return model.CustomerRetrieveResp{
+		Size:      int64(len(c)),
+		Customers: c,
+		Msg:       msg,
+	}
+}
