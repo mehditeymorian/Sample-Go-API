ebb8c3d2f6b2d68720ea6dfb78eb38d21fb49ac5
Mehdi <mehditeymorian322@gmail.com>
2020-12-28 22:06:07 +0330

    add pointer for edit func / remove redundant type for Database var

--- db/DBLayer.go
+++ db/DBLayer.go
@@ -2,12 +2,12 @@ package db
 
 import "GoServer/model"
 
-var Database Layer = CreateListDB()
+var Database = CreateListDB()
 
 type Layer interface {
 	Init()
 	Insert(customer *model.Customer) model.Customer
-	Edit(customer model.Customer) model.Customer
+	Edit(customer *model.Customer) model.Customer
 	Delete() bool
 	RetrieveAll() []model.Customer
 }
