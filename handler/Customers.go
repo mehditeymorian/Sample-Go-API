2fd1553596a94e2fa1fa28c71a38a41e8c99f347
Mehdi <mehditeymorian322@gmail.com>
2020-12-28 22:05:11 +0330

    Complete Retrieval and insertion

--- handler/Customers.go
+++ handler/Customers.go
@@ -1,10 +1,20 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"GoServer/db"
+	"GoServer/model"
+	"GoServer/responses"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
 func CreateCustomer(c echo.Context) error {
-
-	return nil
+	customer := new(model.Customer)
+	if err := c.Bind(customer); err != nil {
+		return err
+	}
+	inserted := db.Database.Insert(customer)
+	return c.JSON(http.StatusOK, responses.EchoResp(inserted, "success"))
 }
 
 func EditCustomer(c echo.Context) error {
@@ -16,5 +26,5 @@ func DeleteCustomer(c echo.Context) error {
 }
 
 func RetrieveCustomer(c echo.Context) error {
-	return nil
+	return c.JSON(http.StatusOK, responses.RetrieveAllResp(db.Database.RetrieveAll(), "success"))
 }
