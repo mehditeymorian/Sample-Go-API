a86078b50d3adaaf699a9362091c76bc099b2675
Mehdi <mehditeymorian322@gmail.com>
2020-12-28 22:08:22 +0330

    change edit func param

--- db/DBLayer.go
+++ db/DBLayer.go
@@ -7,7 +7,7 @@ var Database = CreateListDB()
 type Layer interface {
 	Init()
 	Insert(customer *model.Customer) model.Customer
-	Edit(customer *model.Customer) model.Customer
+	Edit(customerId int64) model.Customer
 	Delete() bool
 	RetrieveAll() []model.Customer
 }
