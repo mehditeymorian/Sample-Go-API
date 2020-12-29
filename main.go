7d973d8cd955004f481b0338ec727d07f31f332d
Mehdi <mehditeymorian322@gmail.com>
2020-12-29 08:34:24 +0330

    add route for report

--- main.go
+++ main.go
@@ -17,5 +17,7 @@ func main() {
 	e.GET("/customers", handler.RetrieveCustomer)
 	e.DELETE("/customers/:cID", handler.DeleteCustomer)
 
+	e.GET("/report/:month", handler.MonthReport)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
