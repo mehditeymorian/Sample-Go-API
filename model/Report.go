93d30162b212a552d950b17f59baa5ab030cdd6c
Mehdi <mehditeymorian322@gmail.com>
2020-12-29 08:53:43 +0330

    create month struct for annual report

--- model/Report.go
+++ model/Report.go
@@ -6,6 +6,11 @@ type MonthlyReport struct {
 	Msg            string `json:"msg"`
 }
 
+type MonthInAnnualReport struct {
+	TotalCustomers int64 `json:"totalCustomers"`
+	Period         int64 `json:"period"`
+}
+
 type AnnualReport struct {
 	Months []MonthlyReport `json:"months"`
 	Msg    string          `json:"msg"`
